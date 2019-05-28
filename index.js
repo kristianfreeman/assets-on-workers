c1ce13a41c5f7c6009fef4a50657c37805ab21a3
Kristian Freeman <kristian@kristianfreeman.com>
2019-05-28 15:16:20 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event))
+})
+
+const BUCKET_NAME = ''
+const BUCKET_URL = `http://storage.googleapis.com/${BUCKET_NAME}`
+
+async function serveAsset(event) {
+  const url = new URL(event.request.url)
+  const cache = caches.default
+  let response = await cache.match(event.request)
+
+  if (!response) {
+    response = await fetch(`${BUCKET_URL}${url.pathname}`)
+    const headers = { 'cache-control': 'public, max-age=14400' }
+    response = new Response(response.body, { ...response, headers })
+    event.waitUntil(cache.put(event.request, response.clone()))
+  }
+  return response
+}
+
+async function handleRequest(event) {
+  if (event.request.method === 'GET') {
+    let response = await serveAsset(event)
+    if (response.status > 399) {
+      response = new Response(response.statusText, { status: response.status })
+    }
+    return response
+  } else {
+    return new Response('Method not allowed', { status: 405 })
+  }
+}
